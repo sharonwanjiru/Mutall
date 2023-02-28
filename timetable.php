655f312b05208847ee4866ac3f4ead4f5c4c052d
sharonwanjiru <sharondichu@gmail.com>
2023-02-28 18:37:22 +0300

    Timetable Repository

--- timetable.php
+++ timetable.php
@@ -27,7 +27,15 @@
             overflow: hidden; 
          }
          .details{
-            border-bottom: 10px
+            border-bottom: 10px;
+            display: flex;
+            gap: 2%;
+            align-items: center;
+         }
+         .det{         
+            display: flex;
+            gap: 2%;
+            align-items: center;
          }
 
         </style>
@@ -39,141 +47,141 @@
 
      <div class="day">
          <h2>Monday</h2>
-
-          <div class="details">
-
-            <div class="imgBx">
-                <div class="a">
+          <!-- Kangara Details -->
+          <div class="details">   
+                <div class="img">
                     <img class="rounded-corners" src="http://drive.google.com/uc?export=view&id=10KYtxL82ANblZbwrAgMc_R7WoHTu8KKD" alt="" 
-                         width="50px" height="50px">
-                </div>
-            </div>
-            <div>
+                        >
+                </div>     
+            <div class="portfolio">
                 <a href="http://206.189.207.206/tracker/portfolio/2023/kangara.html">George Kanga'ra(GK)</a>
-            </div>
-            
+            </div>        
           </div>
-
+            <!-- Andrew Details -->
           <div class="details">
-
-            <div class="imgBx">
-            <div class="left">
+            <div class="img">
                     <img class="rounded-corners" src="http://206.189.207.206/tracker/portfolio/2023/image/drew.jpg" alt="" 
-                         width="50px" height="50px">
+                        >
             </div>
-            <div class="right">
+            <div class="portfolio">
                 <a href="http://206.189.207.206/tracker/portfolio/2023/mugambi.html">Andrew Mugambi(AM)<br></a>
             </div> 
           </div>
-
+           <!-- Isaac Details -->
           <div class="details">
-            <div class="imgBx">
-                <div class="a">
-                    <a href="https://lh3.googleusercontent.com/6RSUmagWNh_WTpC3ttsnCGQy7l3X-hyXmpotYYRI5yufzTYYv3-V0jMD5kaBrcXVoz3aXgTvOqG035xnWPgwRm8IntLM5eczOx-6QFCS4rcw7AK2dwV84OG3_MwHpFQhfe66sEdffw=w2400?source=screenshot.guru"> <img class="rounded-corners" src="https://lh3.googleusercontent.com/6RSUmagWNh_WTpC3ttsnCGQy7l3X-hyXmpotYYRI5yufzTYYv3-V0jMD5kaBrcXVoz3aXgTvOqG035xnWPgwRm8IntLM5eczOx-6QFCS4rcw7AK2dwV84OG3_MwHpFQhfe66sEdffw=w417-h315-p-k" width="50px" height="50px" /> </a>
-                   
-            </div>
-            <a href="">Isaac Waweru(IW)<br></a> 
+                <div class="img">
+                    <a href="https://lh3.googleusercontent.com/6RSUmagWNh_WTpC3ttsnCGQy7l3X-hyXmpotYYRI5yufzTYYv3-V0jMD5kaBrcXVoz3aXgTvOqG035xnWPgwRm8IntLM5eczOx-6QFCS4rcw7AK2dwV84OG3_MwHpFQhfe66sEdffw=w2400?source=screenshot.guru"> <img class="rounded-corners" src="https://lh3.googleusercontent.com/6RSUmagWNh_WTpC3ttsnCGQy7l3X-hyXmpotYYRI5yufzTYYv3-V0jMD5kaBrcXVoz3aXgTvOqG035xnWPgwRm8IntLM5eczOx-6QFCS4rcw7AK2dwV84OG3_MwHpFQhfe66sEdffw=w417-h315-p-k" /> </a>          
+                </div>
+                <div class="portfolio">
+                    <a href="">Isaac Waweru(IW)<br></a> 
+                </Div>
           </div>   
-          </div>   
+            
      </div>
-     </div>
-
+    
+       <!-- Tuesday -->
    
      <div class="day">
         <h2>Tuesday</h2>
-       
+       <!-- Francis' Details -->
         <div class="details">
-            <div class="imgBx">
-                <div class="a">
+            <div class="img">
                     <img class="rounded-corners" src="  http://206.189.207.206/tracker/v/images/frank.jpg" alt="" 
-                         width="50px" height="50px">
+                     >
             </div>
+            <div class="portfolio">
             <a href="http://206.189.207.206/tracker/portfolio/2023/francis.html">Francis Nyakundi(FN)<br></a> 
-          </div>   
+            </div>   
         </div>   
-
+          <!-- George's Details -->
         <div class="details">
-            <div class="imgBx">
-                <div class="a">
+             <div class="img">
                     <a href="https://lh3.googleusercontent.com/x7nJjq2_nEWxboeNnPQq1lqGs55EZGVie8UatieTs31D9LSXZ0whOYX56QHZNpKaJyvBQY5iaT_V4vvVfWEpiGvo_HkBkGV-dX-MWV_LwaSlqtM2WpQBDynL_KZ6EUmgBFMg8j-nww=w2400?source=screenshot.guru"> <img class="rounded-corners" src="https://lh3.googleusercontent.com/x7nJjq2_nEWxboeNnPQq1lqGs55EZGVie8UatieTs31D9LSXZ0whOYX56QHZNpKaJyvBQY5iaT_V4vvVfWEpiGvo_HkBkGV-dX-MWV_LwaSlqtM2WpQBDynL_KZ6EUmgBFMg8j-nww=w600-h315-p-k" width="50px" height="50px"> </a>           
             </div> 
-            <a href="http://206.189.207.206/tracker/portfolio/2023/Munya.html">George Munya(GM)<br></a>
-          </div>   
+            <div class="portfolio">
+                 <a href="http://206.189.207.206/tracker/portfolio/2023/Munya.html">George Munya(GM)<br></a>
+           </div>   
           </div>   
     </div>
-
+ 
+     <!-- Wednesday -->
     <div class="day">
         <h2>Wednesday</h2>
+        <!-- Sharon's details -->
         <div class="details">
-            <div class="imgBx">
-                <div class="a">
-                    <a href="https://lh3.googleusercontent.com/Ffxiv6kqEGP8ixQF1udakKYXQ--cdvQcyc7PlaIMbZT6UZMGZZRscOsfJ7QU7W5kMKSQ29K1WSR8h88INpHbZMRxcByaAYXpj620IuM6Ks7O4FzL40HDI420v29xNTmrRMBEisdPqw=w2400?source=screenshot.guru"> <img class="rounded-corners" src="https://lh3.googleusercontent.com/Ffxiv6kqEGP8ixQF1udakKYXQ--cdvQcyc7PlaIMbZT6UZMGZZRscOsfJ7QU7W5kMKSQ29K1WSR8h88INpHbZMRxcByaAYXpj620IuM6Ks7O4FzL40HDI420v29xNTmrRMBEisdPqw=w600-h315-p-k" width="50px" height="50px" overflow="hidden"> </a>
+            <div class="img">
+                    <a href="https://lh3.googleusercontent.com/Ffxiv6kqEGP8ixQF1udakKYXQ--cdvQcyc7PlaIMbZT6UZMGZZRscOsfJ7QU7W5kMKSQ29K1WSR8h88INpHbZMRxcByaAYXpj620IuM6Ks7O4FzL40HDI420v29xNTmrRMBEisdPqw=w2400?source=screenshot.guru"> <img class="rounded-corners" src="https://lh3.googleusercontent.com/Ffxiv6kqEGP8ixQF1udakKYXQ--cdvQcyc7PlaIMbZT6UZMGZZRscOsfJ7QU7W5kMKSQ29K1WSR8h88INpHbZMRxcByaAYXpj620IuM6Ks7O4FzL40HDI420v29xNTmrRMBEisdPqw=w600-h315-p-k"  overflow="hidden"> </a>
             </div>
+            <div class="porfolio">
             <a href="http://206.189.207.206/tracker/portfolio/2023/wanjiru.html">Sharon Wanjiru(SW)<br></a> 
           </div>   
         </div>   
 
+        <!-- carol's details -->
         <div class="details">
-            <div class="imgBx">
-                <div class="a">
+                <div class="img">
                     <img class="rounded-corners" src="https://drive.google.com/uc?export=view&amp;id=1yWHhNu4NVyPjqmbXOuDDjZL9cPcF2ulH" alt="" 
-                         width="50px" height="50px">
-            </div>
+                        >
+                </div>
+                <div class="porfolio">
             <a href="http://206.189.207.206/tracker/portfolio/2023/wawira.html">Carol Wawira(CW)<br></a> 
           </div>   
         </div>   
     </div>
 
+
+    <!-- Thursday -->
     <div class="day">
         <h2>Thursday</h2>
+        <!-- Joan's details -->
         <div class="details">
-            <div class="imgBx">
-                <div class="a">
+            <div class="img">
                     <img class="rounded-corners" src="https://drive.google.com/uc?export=view&id=1gETpOsBaik0CumLnEZIEG1u4lkgi1ptl" alt="" 
-                         width="50px" height="50px">
+                        >
             </div>
+            <div class="portfolio">
             <a href="http://206.189.207.206/tracker/portfolio/2023/nduta.html">Joan Nduta(JN)<br></a> 
           </div>   
         </div>   
-
+         <!-- James Details -->
         <div class="details">
-            <div class="imgBx">
-                <div class="a">
+                <div class="img">
                     <img class="rounded-corners" src="https://lh3.googleusercontent.com/pw/AMWts8CyI5Nz8iqJYeK5oLBujhR23dm-FV4RtO-TUoxHoVMoT9_J0kpIi5StZsomLL4yeFi68a-fFvjjJashydaAYi4JnUAWYdE6qPGyefQ4yGkoa5_hCqi6B8CY_lO6xb0cg4qo-OEBqy0gU1Rtq74r-lrySQ=w708-h960-s-no?authuser=0" alt="Ndichu James Kiberenge image" width="50px" height="50px">
-            </div>
-            <a href="http://206.189.207.206/tracker/portfolio/2023/kiberenge.html">James Kiberenge(JK)<br></a> 
+               </div>
+            <div class="portfolio">
+                <a href="http://206.189.207.206/tracker/portfolio/2023/kiberenge.html">James Kiberenge(JK)<br></a> 
           </div>   
         </div>   
     </div>
-
+      <!-- Friday -->
     <div class="day">
         <h2>Friday</h2>
-
+       <!-- Peter's details -->
         <div class="details">
-            <div class="imgBx">
-                <div class="a">
+                <div class="img">
                     <img class="rounded-corners" src="https://db3pap003files.storage.live.com/y4mYkTxSEdlNhFREDQ7FZvRrdtMGliIzvjGykN_dx2Y16yNfAx0pECbD0sPnKLa9Xmc4u0E--9P-m5PO02f5A2BiZxJd-Iz8d4FGlhV899pAgFC5tn_p2UIGUfyJUeW0kDeuTEGWqTvbvvb6W2dNW-nVWk-5knHb0dbsIdk21q-g5cI5mQjSMhznMMK1ksAMIWX?width=256&amp;height=256&amp;cropmode=none" width="50px" height="50px">
             </div>
+            <div class="portfolio">
             <a href="http://206.189.207.206/tracker/portfolio/2023/kamau.html">Peter Kamau(PM)<br></a> 
           </div>   
         </div>   
-
+        <!-- Bernards Details -->
         <div class="details">
-            <div class="imgBx">
-                <div class="a">
+                <div class="img">
                     <img class="rounded-corners" src="muli.jpg" alt="" 
-                         width="50px" height="50px">
+                    >
             </div>
+            <div class="portfolio">
             <a href="http://206.189.207.206/tracker/portfolio/2023/muli.html">Bernard Muli(BM)<br></a> 
           </div>   
         </div>   
-
+        <!-- Daniel's Details -->
         <div class="details">
-            <div class="imgBx">
-                <div class="a">
+                <div class="img">
                     <img class="rounded-corners" src="http://206.189.207.206/tracker/portfolio/2023/image/me.jpeg" alt="" 
-                         width="50px" height="50px">
-            </div>
+                         >
+             </div>
+             <div class="porfolio">
             <a href="http://206.189.207.206/tracker/portfolio/2023/kaniu.html">Daniel Kaniu(DN)<br></a> 
           </div>   
         </div>   
